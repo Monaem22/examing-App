7ce5596c7f477fb578f6183f8e40220a9f8ec63f
Yosssef9 <147534893+Yosssef9@users.noreply.github.com>
2025-01-16 01:55:55 +0200

    Add user.route.js with getAllStudents & getOneStudent route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
 const { addStudent } = require("../controllers/student.controller.js");
+const {
+  getAllStudents,
+  getOneStudent,
+} = require("../controllers/student.controller.js");
 const verifyToken = require("../middlewares/verifyToken.js");
 router.post("/add-student", verifyToken, addStudent);
+router.get("/all-Students", getAllStudents);
+router.get("/:id", getOneStudent);
 module.exports = router;
