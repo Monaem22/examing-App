e3fff2d1e09968fd4ec64c9396b3d2e28bc3b553
Yosssef9 <147534893+Yosssef9@users.noreply.github.com>
2025-01-30 23:24:32 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -14,7 +14,14 @@ const app = express();
 dotenv.config();
 dbConnection();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
