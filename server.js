1f2075b11723e3d16e24d7354bdba19cd8cbff59
Elshirbini <ahmedalshirbini33@gmail.com>
2025-03-03 02:04:26 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(
       "https://mahmoud-ebrahim-elazony.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
